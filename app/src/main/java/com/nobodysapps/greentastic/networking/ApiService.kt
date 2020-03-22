f8ccffb1cb2753139f6f1671f1a9c4eb5df84f40
Jonathan Loos <loos.jonathan.martin@gmail.com>
2020-03-22 17:41:25 +0100

    Add ApiService WIP

---
+++ app/src/main/java/com/nobodysapps/greentastic/networking/ApiService.kt
@@ -0,0 +1,13 @@
+package com.nobodysapps.greentastic.networking
+
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("query_autocomplete")
+    fun getAutoComplete(@Query("search_string") searchString: String,
+                        @Query("user_location") userLocation: String=""): Single<List<String>>
+}
+
+//https://clean-commuter.appspot.com/query_autocomplete?user_location=\(ownCoords)&search_string=\(frankfurt)
