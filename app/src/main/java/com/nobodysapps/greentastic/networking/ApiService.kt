3362403a51a210ab621358a0827fbe96d64d2fe4
Jonathan Loos <loos.jonathan.martin@gmail.com>
2020-03-22 17:44:32 +0100

    Make userLocation queryParameter optional

--- app/src/main/java/com/nobodysapps/greentastic/networking/ApiService.kt
+++ app/src/main/java/com/nobodysapps/greentastic/networking/ApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 interface ApiService {
     @GET("query_autocomplete")
     fun getAutoComplete(@Query("search_string") searchString: String,
-                        @Query("user_location") userLocation: String=""): Single<List<String>>
+                        @Query("user_location") userLocation: String?=null): Single<List<String>>
 }
 
 //https://clean-commuter.appspot.com/query_autocomplete?user_location=\(ownCoords)&search_string=\(frankfurt)
