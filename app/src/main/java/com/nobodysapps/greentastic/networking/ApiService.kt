e7eb4f86a108bc9a396ef039b6f58fb1ce9b10d0
Jonathan Loos <loos.jonathan.martin@gmail.com>
2020-03-31 00:17:50 +0200

    Add api call for directions WIP

--- app/src/main/java/com/nobodysapps/greentastic/networking/ApiService.kt
+++ app/src/main/java/com/nobodysapps/greentastic/networking/ApiService.kt
@@ -8,6 +8,15 @@ interface ApiService {
     @GET("query_autocomplete")
     fun getAutoComplete(@Query("search_string") searchString: String,
                         @Query("user_location") userLocation: String?=null): Single<List<String>>
+
+    @GET("query_directions")
+    fun getDirections(@Query("source") source: String,
+                      @Query("destination") destination: String,
+                      @Query("car_type") carType: String?=null,
+                          @Query("car_type") weights: List<String>?=null)
 }
 
-//https://clean-commuter.appspot.com/query_autocomplete?user_location=\(ownCoords)&search_string=\(frankfurt)
+//"/query_directions?source=\(source_)&destination=\(destination)&car_type=\( car_type)&weights=\(time),\(emis),\(cost),\(cals),\(toxc)
+// weights: 0.03,0.1,0,0,0
+//https://clean-commuter.appspot.com/query_autocomplete?user_location=\(ownCoords)&search_string=\(frankfur
+// t)
