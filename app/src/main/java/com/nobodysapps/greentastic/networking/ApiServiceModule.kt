5ce2397f26f25d2fa1195fca03b263ccb5146430
Jonathan Loos <loos.jonathan.martin@gmail.com>
2020-05-03 23:09:07 +0200

    Make ApiService Singleton

--- app/src/main/java/com/nobodysapps/greentastic/networking/ApiServiceModule.kt
+++ app/src/main/java/com/nobodysapps/greentastic/networking/ApiServiceModule.kt
@@ -5,10 +5,13 @@ import dagger.Provides
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 
 @Module
 class ApiServiceModule {
+
+        @Singleton
         @Provides
         fun provideApiService(): ApiService {
                 return Retrofit.Builder()
